318deb8288c71e05ea0323a787c277d7813983ab
Minh Tran <115627819+minhtran5109@users.noreply.github.com>
2023-02-27 16:06:00 +1100

    added square grid

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-let containerDiv = document.createElement('div')
-containerDiv.classList.add('container')
+let containerDiv = document.createElement('div');
+containerDiv.classList.add('container');
+// document.body.appendChild(containerDiv)
+
+let cols = 16;
+let rows = 16;
+
+for (let i = 0; i < cols; i++) {
+    let colDiv = document.createElement('div');
+    colDiv.classList.add('col');
+    for (let j=0; j < rows; j++) {
+        let rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+        rowDiv.textContent = i + '-' +j; // set text
+        colDiv.appendChild(rowDiv);
+    }
+    containerDiv.appendChild(colDiv);
+}
+
+document.body.appendChild(containerDiv);

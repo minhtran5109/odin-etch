5f0f6ba230f7823687a2e8ac5b02aa010eef0712
Minh Tran <115627819+minhtran5109@users.noreply.github.com>
2023-03-02 10:20:20 +1100

    refactor and implemented custom grid functionality

--- script.js
+++ script.js
@@ -2,30 +2,53 @@ let containerDiv = document.createElement('div');
 containerDiv.classList.add('container');
 // document.body.appendChild(containerDiv)
 
-let cols = 16;
-let rows = 16;
-
-for (let i = 0; i < cols; i++) {
-    let colDiv = document.createElement('div');
-    colDiv.classList.add('col');
-    for (let j=0; j < rows; j++) {
-        let rowDiv = document.createElement('div');
-        rowDiv.classList.add('row');
-        rowDiv.textContent = i + '-' +j; // set text
-        colDiv.appendChild(rowDiv);
+const custBtn = document.getElementById("add-btn");
+let size = 16;
+custBtn.onclick = () => {
+    size = parseInt(prompt("Specify grid density"));
+    if (size > 100) {
+        alert("Please specify a number less than or equal 100 only")
+    } else {
+        reset_grid();
+        make_grid(size);
     }
-    containerDiv.appendChild(colDiv);
 }
 
-document.body.appendChild(containerDiv);
+make_grid(size)
+// let cols = nGrid;
+// let rows = nGrid;
+// console.log(nGrid);
 
-let gridList = document.querySelectorAll(".row");
+function make_grid(size) {
+    for (let i = 0; i < size; i++) {
+        let colDiv = document.createElement('div');
+        colDiv.classList.add('col');
+        for (let j=0; j < size; j++) {
+            let rowDiv = document.createElement('div');
+            rowDiv.classList.add('row');
+            //rowDiv.textContent = i + '-' +j; // set text
+            colDiv.appendChild(rowDiv);
+        }
+        containerDiv.appendChild(colDiv);
+    }   
+    document.body.appendChild(containerDiv);
+    apply_brush()
+}
 
-gridList.forEach((grid) => {
-    grid.addEventListener('mouseover', () => {
-        grid.style.backgroundColor = 'red';
+function reset_grid() {
+    while (containerDiv.firstChild) {
+        containerDiv.removeChild(containerDiv.lastChild);
+    }
+}
+
+function apply_brush() {
+   let gridList = document.querySelectorAll(".row");
+    gridList.forEach((grid) => {
+        grid.addEventListener('mouseover', () => {
+            grid.style.backgroundColor = 'red';
+        });
+        // grid.addEventListener('mouseout', () => {
+        //     grid.style.backgroundColor = 'aqua';
+        // });
     });
-    // grid.addEventListener('mouseout', () => {
-    //     grid.style.backgroundColor = 'aqua';
-    // });
-});
+}
